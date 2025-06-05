b9fc0eb64ba661950c3c8970ec2da4215c32f9e5
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-06-05 14:55:18 -0400

    updates

--- home/.config/nvim/lua/plugins/codecompanion.lua
+++ home/.config/nvim/lua/plugins/codecompanion.lua
@@ -2,20 +2,33 @@ return {
 	{
 		"olimorris/codecompanion.nvim",
 		event = "VeryLazy",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
 		opts = {
 			adapters = {
 				openai = function()
 					return require("codecompanion.adapters").extend("openai", {
 						env = {
-							api_key = "OPENAI_API_KEY",
+							api_key = function()
+								return os.getenv("OPENAI_API_KEY")
+							end,
 						},
 					})
 				end,
 			},
-		},
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
+			strategies = {
+				chat = {
+					adapter = "openai",
+				},
+				inline = {
+					adapter = "openai",
+				},
+				cmd = {
+					adapter = "openai",
+				},
+			},
 		},
 	},
 }
