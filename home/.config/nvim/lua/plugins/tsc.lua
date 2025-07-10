8999f3a96d5a48a72f1c3a1f866323bd350da900
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-07-10 07:54:22 -0400

    updates

--- home/.config/nvim/lua/plugins/tsc.lua
+++ home/.config/nvim/lua/plugins/tsc.lua
@@ -5,6 +5,7 @@ return {
 		ft = { "typescript", "typescriptreact" },
 		config = function()
 			require("tsc").setup({
+				bin_path = "/Users/dmmulroy/.bun/bin/tsgo",
 				auto_open_qflist = true,
 				pretty_errors = false,
 			})
