61de2a1145a33b7e683c783a2b0897fc3a1fe735
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-07-21 07:46:14 -0400

    updates

--- home/.config/nvim/lua/plugins/luasnip.lua
+++ home/.config/nvim/lua/plugins/luasnip.lua
@@ -2,7 +2,11 @@ return {
 	{
 		"L3MON4D3/LuaSnip",
 		event = "InsertEnter",
-		build = "make install_jsregexp",
+		build = function()
+			-- Force clean submodule update before building
+			vim.fn.system("cd " .. vim.fn.stdpath("data") .. "/lazy/LuaSnip && git submodule update --init --recursive")
+			require("lazy.util").float_cmd({ "make", "install_jsregexp" }, { cwd = vim.fn.stdpath("data") .. "/lazy/LuaSnip" })
+		end,
 		dependencies = {
 			{
 				"rafamadriz/friendly-snippets",
