681ed3a55b392b0cb5cb4436b7a0087525ae2838
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-11-30 15:23:02 -0500

    migrate to blink

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,14 @@
 return {
 	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	{
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPost" },
 		cmd = { "LspInfo", "LspInstall", "LspUninstall", "Mason" },
@@ -40,10 +49,6 @@ return {
 							runtime = { version = "LuaJIT" },
 							workspace = {
 								checkThirdParty = false,
-								library = {
-									"${3rd}/luv/library",
-									unpack(vim.api.nvim_get_runtime_file("", true)),
-								},
 							},
 							telemetry = { enabled = false },
 						},
@@ -98,7 +103,16 @@ return {
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			-- Use Blink.cmp capabilities if available, fallback to cmp_nvim_lsp
+			local has_blink, blink = pcall(require, "blink.cmp")
+			if has_blink then
+				capabilities = vim.tbl_deep_extend("force", capabilities, blink.get_lsp_capabilities())
+			else
+				local has_cmp, cmp_lsp = pcall(require, "cmp_nvim_lsp")
+				if has_cmp then
+					capabilities = vim.tbl_deep_extend("force", capabilities, cmp_lsp.default_capabilities())
+				end
+			end
 
 			-- Setup LspAttach autocmd for keybindings (replaces on_attach)
 			vim.api.nvim_create_autocmd("LspAttach", {
