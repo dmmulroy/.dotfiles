897b6dbea545ca546c24b3b6fa0bc60f2c82295c
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-11-30 15:23:02 -0500

    migrate to blink

--- home/.config/fish/conf.d/functions.fish
+++ home/.config/fish/conf.d/functions.fish
@@ -1,9 +1,9 @@
 function fvim
     if test (count $argv) -eq 0
-        fd -t f | fzf --header "Open File in Vim" --preview "cat {}" | xargs nvim
+        fd -H -t f | fzf --header "Open File in Vim" --preview "cat {}" | xargs nvim
     else
         set -l query (string join " " $argv)
-        fd -t f | fzf --header "Open File in Vim" --preview "cat {}" -q "$query" | xargs nvim
+        fd -H -t f | fzf --header "Open File in Vim" --preview "cat {}" -q "$query" | xargs nvim
     end
 end
 
