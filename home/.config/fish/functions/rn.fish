005ce346298c496143bc95cb2c4b9d94440f751b
Dillon Mulroy <dillon.mulroy@gmail.com>
2025-12-29 08:14:42 -0500

    fix fish utils: rn, bgr, tempd, scratch per evanhahn blog
    - rn: "right now" time/date display, not rename
    - bgr: background run cmd (replaces wrong bg URL opener)
    - tempd: cd to temp dir, not temp display
    - scratch: simple $EDITOR $(mktemp)

--- home/.config/fish/functions/rn.fish
+++ home/.config/fish/functions/rn.fish
@@ -1,22 +1,5 @@
-function rn -d "Rename files with a friendly interface"
-  if test (count $argv) -lt 2
-    echo "Usage: rn <source> <destination>"
-    echo "Rename files or directories"
-    return 1
-  end
-
-  set -l source $argv[1]
-  set -l dest $argv[2]
-
-  if not test -e $source
-    echo "Error: '$source' does not exist"
-    return 1
-  end
-
-  if test -e $dest
-    echo "Error: '$dest' already exists"
-    return 1
-  end
-
-  mv -v $source $dest
+function rn -d "Right now - display current time and calendar"
+  date "+%l:%M%p on %A, %B %d, %Y" | string trim
+  echo
+  cal
 end
--- home/.config/fish/functions/scratch.fish
+++ home/.config/fish/functions/scratch.fish
@@ -1,33 +1,12 @@
-function scratch -d "Create and open a temporary scratch file"
-  # Create a scratch directory if it doesn't exist
-  set -l scratch_dir /tmp/scratch
-  if not test -d $scratch_dir
-    mkdir -p $scratch_dir
-  end
-
-  # Generate filename
-  set -l timestamp (date +%Y%m%d_%H%M%S)
-  set -l filename
-  
-  if test (count $argv) -gt 0
-    # Use provided name with timestamp
-    set filename "$scratch_dir/$argv[1]_$timestamp"
-  else
-    # Default scratch file name
-    set filename "$scratch_dir/scratch_$timestamp.txt"
-  end
-
-  # Create and open the file
-  touch $filename
-  echo "Created scratch file: $filename"
-  
-  # Open in editor
+function scratch -d "Open a temporary file in editor"
+  set -l tmpfile (mktemp)
   if set -q EDITOR
-    $EDITOR $filename
-  else if command -v nano &>/dev/null
-    nano $filename
+    $EDITOR $tmpfile
+  else if command -v nvim &>/dev/null
+    nvim $tmpfile
+  else if command -v vim &>/dev/null
+    vim $tmpfile
   else
-    echo "File created at: $filename"
-    echo "No editor found. Set \$EDITOR to edit automatically."
+    nano $tmpfile
   end
 end
