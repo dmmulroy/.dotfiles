92ef5a36917c4b7df58693480c291ce023c4f740
Dillon Mulroy <dillon.mulroy@gmail.com>
2026-02-17 14:20:37 -0500

    updates

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -16,5 +16,3 @@ fish_add_path ~/.dotfiles
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
-# Added by Hades
-set -gx PATH $PATH $HOME/.hades/bin
